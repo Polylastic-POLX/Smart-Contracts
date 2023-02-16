aa212620acfb242bd029f388d390cb5862c2c096
gbortnikov <g_bortnikov@list.ru>
2023-02-16 17:48:40 +0700

    contracts

---
+++ contracts/IndexLP.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./interfaces/IERC20BM.sol";
+
+contract IndexLP is ERC20, IERC20BM, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor(address partnerProgram) ERC20("Polylastic LP", "ILP") {
+        _setupRole(MINTER_ROLE, msg.sender);
+        _setupRole(BURNER_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, partnerProgram);
+        _mint(address(this), 0);
+    }
+
+    function mint(address account, uint256 amount)
+        external
+        onlyRole(MINTER_ROLE)
+    {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount)
+        external
+        onlyRole(BURNER_ROLE)
+    {
+        _burn(account, amount);
+    }
+}
